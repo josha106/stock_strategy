acdbde86afed79b5ba4b4706e6dca45c65ce4a44
Kim-Hyeon-Jun <76915090+Kim-Hyeon-Jun@users.noreply.github.com>
2021-01-30 22:16:41 +0900

    proto

--- lib/main.dart
+++ lib/main.dart
@@ -26,92 +26,99 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  var userImageurl =
+      'https://blog.kakaocdn.net/dn/bhk3Sg/btquM9BVthu/1Par8gNR9w6UB96dEQ7k70/img.png';
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.white,
+            leading: Container(
+              margin: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(image: NetworkImage(userImageurl))),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 20),
+                  child: Text(
+                    'Yang Mi',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                )
+              ],
+            )),
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 50),
+              child: Row(
+                children: [
+                  Container(
+                    height: 35,
+                    width: 100,
+                    padding:
+                        EdgeInsets.only(top: 5, bottom: 5, left: 15, right: 15),
+                    decoration: BoxDecoration(color: Color(0xffe3e3e3)),
+                    child: Text('KOSPI'),
+                  ),
+                  Container(
+                    height: 35,
+                    width: 100,
+                    padding:
+                        EdgeInsets.only(top: 5, bottom: 5, left: 15, right: 15),
+                    decoration: BoxDecoration(color: Color(0xffe3e3e3)),
+                    child: Text('KOSDAQ'),
+                  ),
+                  Container(
+                    height: 35,
+                    width: 100,
+                    padding:
+                        EdgeInsets.only(top: 5, bottom: 5, left: 15, right: 15),
+                    decoration: BoxDecoration(color: Color(0xffe3e3e3)),
+                    child: Text('NASDAQ'),
+                  ),
+                ],
+              ),
             ),
+            Container(
+              height: 200,
+              decoration: BoxDecoration(color: Color(0xffe3e3e3)),
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                margin: EdgeInsets.only(top: 20),
+                width: 100,
+                height: 35,
+                decoration: BoxDecoration(
+                    color: Color(0xffe3e3e3),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    )),
+                child:
+                    Center(child: Text('검색', style: TextStyle(fontSize: 18))),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
